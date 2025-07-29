ce54ffb505cc4eedac541b545b3379304508a5a3
drizz27 <johngalang1000@gmail.com>
2025-07-29 19:28:18 +0800

    callback inayos ko dinagdagan ng oauth handler

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -7,12 +7,8 @@
 
 <script>
 export default {
-  // The auth module automatically handles the callback when this page is loaded.
-  // You don't usually need extra logic here for the auth module itself.
-  // You might add a loading spinner or a message.
-  mounted() {
-    // You can add a small delay or a check if the auth process completed
-    // For example, if(!this.$auth.loggedIn) { // redirect to login or show error }
+  async mounted() {
+    await this.$auth.handleCallback()
   }
 }
 </script>
