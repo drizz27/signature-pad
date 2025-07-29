e3db84a335247a167219112637c421381383ce79
drizz27 <johngalang1000@gmail.com>
2025-07-29 22:34:01 +0800

    saba

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -6,7 +6,7 @@
 export default {
   async mounted () {
     try {
-      const strategy = this.$route.query.provider || 'discord'
+      const strategy = this.$route.query.provider
       await this.$auth.loginWith(strategy)
       this.$router.replace('/signature')
     } catch (err) {
